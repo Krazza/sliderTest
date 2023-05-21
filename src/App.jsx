efcf3dd9c7dd7ad55478aea128a81fb65176542f
Emerson <muondove@gmail.com>
2023-05-21 21:46:15 +0300

    testing out mui slider

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import RangeSlider from "./components/RangeSlider";
+import './components/styles/App.css';
+
+function App() {
+	return (
+		<div className="container">
+			<RangeSlider/>
+		</div>
+	);
+}
+
+export default App;
